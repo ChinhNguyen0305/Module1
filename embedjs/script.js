0273e587821e86ad58eed87f6f3673a9acd65791
VO HUU NHAT DUC <vohuunhatduc@VOs-MacBook-Air.local>
2021-07-01 13:43:39 +0700

    Update simple js

---
+++ embedjs/script.js
@@ -0,0 +1,6 @@
+
+function greeting() {
+    document.getElementById('greetingButton').value = 'Show me !';
+    document.getElementById('greetingButton').style.backgroundColor = `rgb(181, 234, 234)`;
+
+}
