57d607f7e4b3a2144ebada6079eb7e9c51de5c0f
VO HUU NHAT DUC <vohuunhatduc@VOs-MacBook-Air.local>
2021-07-01 14:52:06 +0700

    update use operator

---
+++ useoperator/function.js
@@ -0,0 +1,5 @@
+function calculate() {
+    var height = document.getElementById('height').value;
+    var width = document.getElementById('width').value;
+    document.getElementById('result').value = +height * +width
+}
