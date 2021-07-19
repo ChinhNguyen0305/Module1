3b21929abe350f3049b11ff5a5e1950769846038
VO HUU NHAT DUC <vohuunhatduc@VOs-MacBook-Air.local>
2021-07-19 17:18:57 +0700

    commit

---
+++ lesson17/script2.js
@@ -0,0 +1,40 @@
+const speed = 20;
+class hero {
+    constructor(img, top, left, size) {
+        this.img = img;
+        this.top = top;
+        this.left = left;
+        this.size = size;
+    }
+    getHeroElement() {
+        return `<img  height="${this.size}" width ="${this.size}" style="position: absolute;top: ${this.top}px;left: ${this.left}px;" src ="${this.img}">`
+    }
+    moveRight() {
+        this.left += speed;
+    }
+    moveLeft() {
+        this.left -= speed;
+    }
+    moveUp() {
+        this.top -= speed;
+    }
+    moveDown() {
+        this.top += speed;
+    }
+}
+
+var doremon = new hero(`Doraemon.jpeg`, 0, 0, 100);
+function start() {
+    if (doremon.left < window.innerWidth - doremon.size && doremon.top < doremon.size) {
+        doremon.moveRight();
+    } else if (doremon.top < window.innerHeight - doremon.size && doremon.left >= window.innerWidth - doremon.size) {
+        doremon.moveDown();
+    } else if (doremon.left > 0) {
+        doremon.moveLeft();
+    } else {
+        doremon.moveUp();
+    }
+    document.getElementById('game').innerHTML = doremon.getHeroElement();
+    setTimeout(start, 100)
+}
+start();
